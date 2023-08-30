ca4c5dd507c65555f1173be62d3759c8f4fb57c3
vgshenshin <vg.shenshin@gmail.com>
2023-08-30 16:30:07 +0300

    add Redux Toolkit: configureStore()

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit'
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const store = createStore( 
-	combineReducers({ heroes, filters }), 
-	compose(
-		applyMiddleware(thunk), 
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
-);
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+};
+
+const store = configureStore({
+	reducer: { heroes, filters },
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(stringMiddleware),
+	devTools: process.env.NODE_ENV !== 'production'
+})
 
 export default store;
